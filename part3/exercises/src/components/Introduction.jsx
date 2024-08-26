2e4e3732f8fc8d089e9aa8d745ca8174a31ed2ae
Jeffery Phipps <jefferyphipps@att.net>
2024-08-26 12:57:39 -0500

    worked on exercises for part 3

--- part3/exercises/src/components/Introduction.jsx
+++ part3/exercises/src/components/Introduction.jsx
@@ -1,11 +1,11 @@
 export default function HobbyIntroduction () {
    return(
       <div>
-         <h2>3 Fun Facts About Needlepoint</h2>
+         <h2>3 Fun Facts About Video Games</h2>
          <ol>
-            <li>Needlepoint uses a specialized canvas as it's base.</li>
-            <li>The most basic stitch is called a tent stitch.</li>
-            <li>Needlepoint uses a lot of different kinds of fibers.</li>
+            <li>Some games use hand eye coordination</li>
+            <li>Games can be party games or online with others</li>
+            <li>Games can help stimulate brain activity.</li>
          </ol>
       </div>
    );
